4387c03cf666c733534b79c694671032f917bdce
ungericw <ungericw@tutanota.com>
2019-04-16 10:28:45 -0500

    Add project files.

---
+++ purplewellfaeries/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace purplewellfaeries.Models
+{
+  public class ErrorViewModel
+  {
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}
